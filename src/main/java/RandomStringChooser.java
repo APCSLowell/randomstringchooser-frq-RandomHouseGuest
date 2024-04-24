ccbf6d886f205338be1ec488de0a3fc0bf15e97f
Kyle Yuen <103063168+RandomHouseGuest@users.noreply.github.com>
2024-04-24 09:10:32 -0700

    RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,8 +1,17 @@
 import java.util.*;
 public class RandomStringChooser
 {
-  /* to be implemented in part (a) */
-  
-  //Heads up! 
-  //You will get a very confusing error message until you have working code in part b as well
+  private String[] words;
+  public RandomStringChoose(String [] string){
+    words = string;
+  }
+  public String getNext(){
+    int index = -1;
+    if(index >= words.length){
+      return "NONE";
+    } else{
+      index++;
+      return words[index];
+    }
+  }
 }
